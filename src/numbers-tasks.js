00c0bd165ce302e194b81bf20aa91ee193c8baa8
pbhoper <grigand207@gmail.com>
2025-09-26 20:30:16 +0300

    feat: change files

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -594,7 +594,8 @@ function getMaxNumber(firstNumber, secondNumber) {
 function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const random = Math.random() * (max - min) + min;
+  return Math.round(random);
 }
 
 /**

c63cc6442c08e7a6bd39213b3b8c77ed2f18807c
pbhoper <grigand207@gmail.com>
2025-09-26 20:20:35 +0300

    feat: change files

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -470,8 +470,9 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  const parsed = parseInt(str, base);
+  return Number.isNaN(parsed) ? NaN : parsed;
 }
 
 /**
